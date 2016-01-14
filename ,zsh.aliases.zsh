708b2ff0f21250a6bd00386d1d540543b24e0258
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2016-01-14 11:58:36 -0500

    aliases for search, fix mouse on tmux 2.1

--- ,zsh.aliases.zsh
+++ ,zsh.aliases.zsh
@@ -2,3 +2,9 @@ hash -d r="$HOME/repos"
 hash -d w="$HOME/work"
 
 alias dm='docker-machine'
+
+# mdfind in current director omitting excludes
+ff () { mdfind -onlyin . $* | grep -v -f ~/.search-exclude.txt }
+
+# grep excluding common code library locations
+ffg () { grep -R --exclude-dir=vendor --exclude-dir=node_modules $* . }
