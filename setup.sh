206d486ecddb2f83b17d3fb36bbe64e64696d902
david hamp-gonsalves <dhamp-gonsalves@gitlab.com>
2024-06-19 16:25:43 -0300

    path in title

--- setup.sh
+++ setup.sh
@@ -14,5 +14,6 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 0.9
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
 
 echo "If everthing looks good, run install.sh"
